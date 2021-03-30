5a8c19cc5e3214d994d7d321a1f88c941e90ad1b
SovPho <sovereignphobia@gmail.com>
2021-03-30 12:27:56 -0500

    Basic prime number checker.

---
+++ primes/fermat_primality.py
@@ -0,0 +1,23 @@
+
+import random
+
+def primeTest(testNum):
+  
+  if (testNum % 2 == 0):
+    return False
+    
+  upper_bound = testNum - 1
+  witness = random.randrange(1, upper_bound, 1)
+  
+  # Do five iterations to weed out pseudoprimes
+  isPrime = False
+  i = 1
+  while i < 5:
+    if (witness ** (testNum - 1) % testNum == 1):
+      isPrime = True
+    else: isPrime = False
+    
+    witness = random.randrange(1, upper_bound, 1)
+    i += 1
+  
+  return isPrime
