e3b03b1e2a042170ea6ec5d45c948849b21632c7
GUARDIAN\nicod <nar110@txstate.edu>
2021-04-06 18:25:51 -0500

    no message

--- crypt/bin/main.py
+++ crypt/bin/main.py
@@ -2,11 +2,16 @@ import rsa
 
 
 
-(bob_pub, bob_priv) = rsa.newkeys(256)
-message = 'hello Bob!'.encode('utf8')
+(bob_pub, bob_priv) = rsa.newkeys(1024)
 
+message = 'hello Bob!'.encode('utf8')
 crypto = rsa.encrypt(message, bob_pub)
 
+
 message = rsa.decrypt(crypto, bob_priv)
+
+
+print(bob_pub)
 print(bob_priv)
+print(crypto)
 print(message.decode('utf8'))
