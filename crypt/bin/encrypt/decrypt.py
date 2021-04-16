fc41629a991d8babfd742bf88b43f3ea7555810b
GUARDIAN\nicod <nar110@txstate.edu>
2021-04-16 16:51:56 -0500

    Impelented de/encrypt

--- crypt/bin/encrypt/decrypt.py
+++ crypt/bin/encrypt/decrypt.py
@@ -1,6 +1,10 @@
+import rsa
 
 def decryptmsg(privatekey,msg):
+    # recevered priv key and msg, return decrypted msg
 
-    #recevered public key and msg, return decrypted msg
+    msg = rsa.decrypt(msg, privatekey)
+    msg = msg.decode('utf8')
 
     return msg
+
--- crypt/bin/encrypt/encrypt.py
+++ crypt/bin/encrypt/encrypt.py
@@ -1,7 +1,9 @@
-
+import rsa
 
 def encryptmsg(publickey,msg):
-
     #recevered public key and msg, return encrypted msg
+
+    msg = msg.encode('utf8')
+    msg = rsa.encrypt(msg,publickey)
 
     return msg
