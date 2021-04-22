f66bda3a7b9f9f6df8aecb807ed63e802e84a762
Trey <tht13@txstate.edu>
2021-04-21 19:06:31 -0500

    Function updates

--- crypt/bin/database interface/db interface.py
+++ crypt/bin/database interface/db interface.py
@@ -6,14 +6,14 @@ username = 'vulcans'
 password = 'Vulcanproject2021'
 
 cnxn = pyodbc.connect(
-    'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+    'DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
 cursor = cnxn.cursor()
 
 
 def user_add(user_name, public_key):
     # check if user_name already exists in database
     check_username = cursor.execute(
-        'SELECT user_name FROM user WHERE user_name = %(public_key)s', (public_key,))
+        "SELECT user_name FROM user WHERE user_name=%s",(public_key,))
     check_username = cursor.fetchone()
     if check_username != 0:
         id = cursor.execute(
@@ -22,7 +22,8 @@ def user_add(user_name, public_key):
         id = id + 1
         check_username = cursor.fetchone()
         cursor.execute(
-            "INSERT INTO [dbo].[user] ([id],[user_name], [public_key]) VALUES (id, user_name, public_key)")
+            "INSERT INTO [dbo].[user] ([id],[user_name], [public_key]) VALUES (%s, %s, %s)",
+            (id, user_name, public_key))
         print('User added')
     else:
         print('user_name already exists in database')
@@ -30,7 +31,7 @@ def user_add(user_name, public_key):
 
 def user_login (public_key):
     check_login = cursor.execute(
-        'SELECT user_name FROM user WHERE user_name = %(user_name)s, public_key = %(public_key)',
+        "SELECT user_name FROM user WHERE user_name = %s, public_key = %s",
         (user_name, public_key))
     check_login = cursor.fetchone()
     if check_login != 0:
@@ -43,5 +44,3 @@ def get_messages(public_key):
 def send_messages(public_key):
 
 def change_user_name(new_user_name, public_key):
-
-    
