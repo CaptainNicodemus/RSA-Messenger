faef0429e7a01aaef2b1107e14b3823b9bdbbf67
GUARDIAN\nicod <nar110@txstate.edu>
2021-04-23 20:37:52 -0500

    Added google interaface base funtions

---
+++ crypt/bin/database_interface/google_sheet_interface.py
@@ -0,0 +1,13 @@
+
+
+
+def send_message(sender_pubKey, encrypted_message, receiver_pubKey):
+
+    decrypted_message = encrypted_message
+    return decrypted_message
+
+def pull_message(pubKey):
+
+    my_messages = 0
+    return my_messages
+
