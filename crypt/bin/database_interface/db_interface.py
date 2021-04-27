ef7e6253474274ea7ef1bcdafec5d7d3884dcc6d
rai13 <sovereignphobia@gmail.com>
2021-04-26 21:36:17 -0500

    Updated db_interface

--- crypt/bin/database_interface/db_interface.py
+++ crypt/bin/database_interface/db_interface.py
@@ -1,20 +1,47 @@
-from bin.database_interface.googleSheetToPython import add_user_name_update, add_message
+import googleSheetToPython as gstp
+import pandas as pd
 
 
-def send_message(sender_pubkey, encrypted_messageR, encrypted_messageS, receiver_pubKey):
-    add_message(sender_pubkey, encrypted_messageR, encrypted_messageS, receiver_pubKey)
+class db_interface:
+  
+  # Constructor to get the table and set the columns
+  def __init__(self):
+      self.init_table = gstp.get_all_val()
+      self.dataframe = pd.DataFrame(init_table)
+      self.columns = dataframe[0]
+      
+  
+  def send_message(sender_pubkey, encrypted_messageR, encrypted_messageS, receiver_pubKey):
+      gstp.add_message(sender_pubkey, encrypted_messageR, encrypted_messageS, receiver_pubKey)
+      return
+
+  def user_name_update(pubKey, new_username):
+      gstp.add_user_name_update(pubKey, new_username)
+      return
+
+  def pull_message(pubKey):
+      my_messages = key_name_search(pubKey)
+      return my_messages
+
+  # Search for a username based on pubKey
+  def key_name_search(pubKey):
+   
+     
+    
+    return #username
+
+  def time_sort(): 
+    #sort table by time
     return
-
-def user_name_update(pubKey, new_username):
-    add_user_name_update(pubKey, new_username)
+      
+  def name_sort(): 
+    #sort by name
     return
-
-def pull_message(pubKey):
-    my_messages = 0
-    return my_messages
-
-# Search for a username based on pubKey
-def key_name_search(pubKey):
-
-def get_message_table():
-def get_names_table():
+    
+  def get_message_table():
+    #
+    return
+    
+  def get_names_table():
+    #
+    return
