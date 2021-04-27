334298e7d86d8132a964a1b14333a51066e1d3aa
rai13 <rai13@txstate.edu>
2021-04-27 13:04:21 -0500

    Updated db_interface

--- crypt/bin/database_interface/db_interface.py
+++ crypt/bin/database_interface/db_interface.py
@@ -3,18 +3,13 @@ import pandas
 import googleSheetToPython as gstp
 import pandas as pd
 
+
+# Setting up the dataframe
 init_table = gstp.get_all_val()
 dataframe = pd.DataFrame(init_table)
 dataframe = dataframe.rename(columns={0: 'Time', 1: 'Public Key Sender', 2: 'Public Key Receiver', 3: 'MessageR',
                           4: 'MessageS', 5: 'Sender User Name'})
 dataframe = dataframe.drop(0)
-
-# Constructor to get the table and set the columns
-#def __init__(self):
-#  self.init_table = gstp.get_all_val()
-#  self.dataframe = pd.DataFrame(init_table)
-#  self.columns = dataframe[0]
-#  return 0
 
 def send_message(sender_pubkey, encrypted_messageR, encrypted_messageS, receiver_pubKey):
   gstp.add_message(sender_pubkey, encrypted_messageR, encrypted_messageS, receiver_pubKey)
@@ -36,19 +31,17 @@ def key_name_search(pubKey):
       return dataframe.iloc['5', i]
     i += 1
 
-def message_search(pubKey):
+def get_messages(pubKey):
   messages = pandas.DataFrame
-  messages.columns = ["Received, Sent"]
-  db_index = 0
-  messages_index = 0
-  for key in dataframe['1']:
-    if key == pubKey:
-      messages[messages_index, "Received"]._set_value(dataframe.iloc[db_index]['3'])
-      messages[messages_index, "Sent"]._set_val(dataframe.iloc[db_index]['4'])
-      messages_index += 1
-    db_index += 1
-  return messages
+  messages.columns = ["Time", "Received", "Sent"]
 
+  sent_messages = pd.DataFrame(dataframe.loc[dataframe['Public Key Sender'] == pubKey])
+
+  print(sent_messages)
+
+  received_messages = pd.DataFrame(dataframe.loc[dataframe['Public Key Receiver'] == pubKey])
+
+  print(received_messages)
 
 def time_sort():
   #sort table by time
